167f387e28f23456f0ef311dcb271c5bc9490d37
Tigran Khanaghyan <arm.mail1991@gmail.com>
2021-09-08 18:30:24 +0400

    docs: add GameInfo component

--- src/components/Game/Game.jsx
+++ src/components/Game/Game.jsx
@@ -1,7 +1,8 @@
 import GameContent from "../GameContent/GameContent";
 import GameMode from "../GameMode/GameMode";
+import GameInfo from "../GameInfo/GameInfo";
 import Layout from "../Layout/Layout";
 
 export default function Game() {
-  return <Layout child={<GameContent />} left={<GameMode/>}/>;
+  return <Layout child={<GameContent />} left={<GameMode/>} right={<GameInfo/>}/>;
 }
---
+++ src/components/GameInfo/GameInfo.jsx
@@ -0,0 +1,14 @@
+import './GameInfo.style.css'
+
+export default function GameInfo() {
+  return (
+    <>
+      <div className='title'>
+        <p>Players</p>
+        <p>Score</p>
+      </div>
+      <div></div>
+      <div></div>
+    </>
+  );
+}
