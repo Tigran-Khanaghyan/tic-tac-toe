11c57def80065a39aeeef0c40c261e78e5b3f05d
Tigran Khanaghyan <arm.mail1991@gmail.com>
2021-09-08 16:07:24 +0400

    docs: add Cell component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import Welcome from './components/Welcome/Welcome'
 
 function App() {
   return (
     <div className="App">
-      Hello
+      <Welcome/>
     </div>
   );
 }
---
+++ src/components/Cell/Cell.jsx
@@ -0,0 +1,5 @@
+import "./Cell.style.css";
+
+export default function Cell() {
+  return <div className="cell-container"></div>;
+}
